deca1ce0fdbcabc4df6916d7753e34f6fba6a177
sagbonkh <sidneyagbonkhese9@gmail.com>
2024-03-17 18:00:04 -0700

    Added tablet and desktopdisplay to upload page

--- src/pages/UploadPage/UploadPage.js
+++ src/pages/UploadPage/UploadPage.js
@@ -12,40 +12,48 @@ function UploadPage() {
     <main className="upload-pg">
       <h1 className="upload-pg-title">Upload Video</h1>
       <form className="upload-pg__form">
-        <label>VIDEO THUMBNAIL</label>
-        <img src={uploadImg} alt="runner" className="upload-pg__form-img" />
-        <label htmlFor="upload-pg-input" className="upload-pg__form-label">
-          TITLE YOUR VIDEO
-        </label>
-        <input
-          type="text"
-          className="upload-pg__form-text"
-          placeholder="Add a title to your video"
-          name="upload-pg-input"
-        ></input>
-        <label htmlFor="upload-pg-area" className="upload-pg__form-label">
-          ADD A VIDEO DESCRIPTION
-        </label>
-        <textarea
-          type="text"
-          name="upload-pg-area"
-          className="upload-pg__form-area"
-          placeholder="Add a description to your video"
-        ></textarea>
+        <div className="upload-pg__form-thumbnail-div">
+          <label>VIDEO THUMBNAIL</label>
+          <img src={uploadImg} alt="runner" className="upload-pg__form__img" />
+        </div>
+        <div className="upload-pg__form-text-sect">
+          <label htmlFor="upload-pg-input" className="upload-pg__form-label">
+            TITLE YOUR VIDEO
+          </label>
+          <input
+            type="text"
+            className="upload-pg__form-text"
+            placeholder="Add a title to your video"
+            name="upload-pg-input"
+          ></input>
+          <label htmlFor="upload-pg-area" className="upload-pg__form-label">
+            ADD A VIDEO DESCRIPTION
+          </label>
+          <textarea
+            type="text"
+            name="upload-pg-area"
+            className="upload-pg__form-area"
+            placeholder="Add a description to your video"
+          ></textarea>
+        </div>
       </form>
-      <div className="upload-pg__btn">
-        <img
-          src={publishIcon}
-          alt="upload icon"
-          className="upload-pg__btn-icon"
-        />
-        <button type="submit" onClick={handleSubmit}>
-          <Link to="/" className="link">PUBLISH</Link>
-        </button>
-      </div>
-      <Link to="/" className="upload-pg-cancel">
-        CANCEL
-      </Link>
+      <section className="button-sect">
+        <div className="upload-pg__btn">
+          <img
+            src={publishIcon}
+            alt="upload icon"
+            className="upload-pg__btn-icon"
+          />
+          <button type="submit" onClick={handleSubmit}>
+            <Link to="/" className="link">
+              PUBLISH
+            </Link>
+          </button>
+        </div>
+        <Link to="/" className="upload-pg-cancel">
+          CANCEL
+        </Link>
+      </section>
     </main>
   );
 }
